f30f316bfc5b12986b18c60a60c7362ee04ee4a8
Bradley Adams <brad@bradleyadams.com>
2026-01-30 21:44:57 -0800

    Expanded on Plant Data

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -9,7 +9,7 @@ public class Main : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(plantData.test);
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlantData.cs
+++ Assets/Scripts/PlantData.cs
@@ -1,8 +1,48 @@
+using System;
 using UnityEngine;
+
+[Serializable]
+public enum SoilType
+{
+    Sand,
+    Dirt,
+    Water,
+    Hair,
+}
+
+[Serializable]
+public enum ActivityType
+{
+    Water,
+    Dry,
+    Light,
+    Dark,
+    Loud,
+    Quiet,
+    Hot,
+    Cold,
+}
+
+[Serializable]
+public struct GrowthStage
+{
+    public ActivityType[] activityGoals;
+
+    public GameObject[] parts;
+}
 
 [CreateAssetMenu(fileName = "PlantData", menuName = "Scriptable Objects/Plant Data")]
 public class PlantData : ScriptableObject
 {
     [SerializeField]
-    public string test = "";
+    public GameObject plantPrefab;
+
+    [SerializeField, Range(0f, 30f)]
+    public float growSecondsPerStage = 30f;
+
+    [SerializeField]
+    public GrowthStage[] growthStages;
+
+    [SerializeField]
+    public SoilType soilType;
 }
