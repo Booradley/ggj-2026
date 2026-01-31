35a1529987599f76996445cfe80b9f167dc583b9
Bradley Adams <brad@bradleyadams.com>
2026-01-30 20:42:36 -0800

    Added plant data

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,11 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    [SerializeField]
+    public PlantData plantData;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Test");
+        Debug.Log(plantData.test);
     }
 
     // Update is called once per frame
